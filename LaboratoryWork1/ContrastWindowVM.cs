cdad8be2f24c29d01a40e369e1eb43c780fa7f02
Dyagay Alexandra <dyagaay@gmail.com>
2021-10-15 19:10:27 +0700

    Добавлены некоторые классы

---
+++ LaboratoryWork1/ContrastWindowVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
+using LaboratoryWork1.Service;
+
+namespace LaboratoryWork1
+{
+    public class ContrastWindowVM
+    {
+        public RelayCommand OKCommand { get; set; }
+
+        public RelayCommand CancelCommand { get; set; }
+
+        public ContrastWindowVM(IContrastWindowService contrastWindowService)
+        {
+            OKCommand = contrastWindowService.OKCommand;
+            CancelCommand = contrastWindowService.CancelCommand;
+        }
+    }
+}
---
+++ LaboratoryWork1/Service/IContrastWindowService.cs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
+
+namespace LaboratoryWork1.Service
+{
+    public interface IContrastWindowService
+    {
+        /// <summary>
+        /// DialogResult.
+        /// </summary>
+        bool DialogResult { get; set; }
+
+        /// <summary>
+        /// Opens a window.
+        /// </summary>
+        /// <param name="note">Note</param>
+        void Open(ContrastWindowVM note);
+
+        /// <summary>
+        /// Returns and sets OK command.
+        /// </summary>
+        RelayCommand OKCommand { get; set; }
+
+        /// <summary>
+        /// Returns and sets Cancel command.
+        /// </summary>
+        RelayCommand CancelCommand { get; set; }
+    }
+}
